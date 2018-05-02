76ae64596cc53fb1d4b7b082a381d2ecd9789f85
Ben Thomasson <bthomass@redhat.com>
2018-05-02 14:54:13 -0400

    Fixing up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-var webpack = require('webpack');
+const path = require('path');
+const webpack = require('webpack');
+const SRC_PATH = path.resolve(__dirname, 'src');
+const NETWORK_UI_PATH = SRC_PATH;
 module.exports = {
     entry: {
         app: "./src/main.js",
@@ -13,8 +16,56 @@ module.exports = {
         path: __dirname + "/js",
         filename:  "bundle.js",
     },
+    module: {
+        rules: [
+             {
+                  test: /\.dfdfdfjs$/,
+                  use: {
+                      loader: 'istanbul-instrumenter-loader',
+                      options: { esModules: true }
+                  },
+                  enforce: 'pre',
+                  include: [
+                      /src\//
+                  ]
+              },
+              {
+                  test: /\.js$/,
+                  loader: 'babel-loader',
+                  exclude: /node_modules/,
+                  options: {
+                      presets: [
+                          ['env', {
+                              targets: {
+                                  browsers: ['last 2 versions']
+                              }
+                          }]
+                      ]
+                  }
+              },
+              {
+                  test: /\.html$/,
+                  use: ['ngtemplate-loader', 'html-loader'],
+                  include: [
+                      /src\//
+                  ]
+              },
+              {
+                  test: /\.svg$/,
+                  use: ['ngtemplate-loader', 'html-loader'],
+                  include: [
+                      /src\//
+                  ]
+              }
+        ]
+    },
     plugins: [
          new webpack.ProvidePlugin({Hamster: 'hamsterjs'}),
          new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
-    ]
+    ],
+    resolve: {
+        alias: {
+            '~network-ui': NETWORK_UI_PATH,
+        }
+    }
 };
