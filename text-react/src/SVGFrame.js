cf6a8cb5e638e0048e68869d7ace26f60fb35f6a
Ben Thomasson <ben.thomasson@gmail.com>
2018-09-20 06:38:17 -0400

    Adds text fsm to the component

--- text-react/src/SVGFrame.js
+++ text-react/src/SVGFrame.js
@@ -3,6 +3,8 @@ import Debug from './Debug'
 import Cursor from './Cursor'
 import TextInput from './TextInput'
 import fsm from './fsm'
+import models from './models'
+import util from './util'
 
 class SVGFrame extends Component {
 
@@ -14,14 +16,29 @@ class SVGFrame extends Component {
       lastKey: '',
       frameNumber: 0,
       cursorTransform: '',
-      showDebug: true
+      showDebug: true,
     };
 
+    this.trace_order_seq = util.natural_numbers(0);
+
+    this.future_messages = [];
+
+    this.text = new models.TextInput(1, "Hello World", this, this);
+
+    this.first_channel = new fsm.Channel(null,
+                                         this.text.fsm,
+                                         this);
+
     this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
     this.onMouseWheel = this.onMouseWheel.bind(this);
     this.timer = this.timer.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
     this.onResize = this.onResize.bind(this);
+  }
+
+  send_trace_message (message) {
+    console.log(message);
   }
 
   onMouseMove(e) {
@@ -34,11 +51,23 @@ class SVGFrame extends Component {
     e.preventDefault();
   }
 
+  onMouseDown(e) {
+    this.setState({
+      cursorPosX: e.pageX,
+      cursorPosY: e.pageY,
+      cursorTransform: 'translate(' + e.pageX + ',' + e.pageY + ')'
+    });
+    this.first_channel.send('MouseDown', e);
+
+    e.preventDefault();
+  }
+
   onMouseWheel(e) {
     e.preventDefault();
   }
 
   onKeyDown(e) {
+    this.first_channel.send('KeyDown', e);
     this.setState({
       lastKey: e.key
     });
@@ -66,9 +95,9 @@ class SVGFrame extends Component {
   }
 
   componentDidMount() {
-     var intervalId = setInterval(this.timer, 17);
+     //var intervalId = setInterval(this.timer, 17);
      this.setState({
-       intervalId: intervalId,
+     //  intervalId: intervalId,
        frameWidth: window.innerWidth,
        frameHeight: window.innerHeight
      });
@@ -87,7 +116,7 @@ class SVGFrame extends Component {
         height={this.state.frameHeight}
         width={this.state.frameWidth}
         onMouseMove={this.onMouseMove}
-        onMouseDown={this.onMouseMove}
+        onMouseDown={this.onMouseDown}
         onMouseUp={this.onMouseMove}
         onMouseEnter={this.onMouseMove}
         onMouseLeave={this.onMouseMove}
@@ -95,7 +124,7 @@ class SVGFrame extends Component {
         >
           <Debug {...this.state} />
           <Cursor transform={this.state.cursorTransform} />
-          <TextInput />
+          <TextInput {...this.text}/>
         </svg>
       </div>
     );
--- text-react/src/TextInput.js
+++ text-react/src/TextInput.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 
 import Colors from './Colors'
 
+
 class TextInput extends Component {
   constructor (props) {
     super(props);
 
     this.state = {
-      transform: 'translate(200,200)',
-      text_selected: true,
-      text_show_cursor: true,
+      transform: 'translate(400,400)',
+      text_selected: false,
+      text_show_cursor: false,
       text_width: 100,
       text_cursor_pos: 90,
-      text_value: 'Hello World',
-      text_text_id: 'test_1'
+      text_value: this.props.text_value,
+      text_text_id: 'text_1',
     };
   }
   render() {
@@ -52,7 +53,8 @@ class TextInput extends Component {
                 style={textInputBoxStyle}
                 rx='2'>
           </rect>
-        (this.state.text_selected ?
+      {this.props.selected ?
+        <g>
           <rect x='0' y='0'
                 width={this.state.text_width} height='20'
                 style={selected2Style}
@@ -63,21 +65,22 @@ class TextInput extends Component {
                 style={selected1Style}
                 rx='4'>
           </rect>
-        : null)
-        (this.state.text_selected ?
+        </g>
+        : null}
+      {this.props.text_selected ?
           <rect x='2' y='2'
-                width={this.state.text_cursor_pos} height='16'
+                width={this.props.cursor_pos} height='16'
                 style={textSelectedStyle}>
           </rect>
-        : null)
-        (this.state.text_show_cursor ?
-          <line x1={this.state.text_cursor_pos} x2={this.state.text_cursor_pos}
+        : null}
+      {this.props.show_cursor ?
+          <line x1={this.props.cursor_pos} x2={this.props.cursor_pos}
                 y1='2' y2='18'
                 style={cursorStyle}></line>
-        : null )
+        : null }
         <text id={this.state.text_text_id}
               x='5' y='15'
-              style={textInputStyle}>{this.state.text_value}</text>
+              style={textInputStyle}>{this.props.value}</text>
       </g>
     );
   }
